faf96d52bec478592410f05522d3203daca3152d
Devrath Rathee <devrathratheedev@Devraths-MacBook-Pro.local>
2022-11-16 14:52:16 +0530

    initial commit

---
+++ Jurni/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jurni
+//
+//  Created by Devrath Rathee on 16/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
