a6d58219f2b5d3b11fc83100d3a9d921601ff5d6
Devrath <devrathtoolboxos.com>
2022-12-09 00:07:10 +0530

    added container and scroll view for contact and payment details controller

---
+++ Jurni/PaymentDetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PaymentDetailsViewController.swift
+//  Jurni
+//
+//  Created by Devrath Rathee on 08/12/22.
+//
+
+import UIKit
+
+class PaymentDetailsViewController: UIViewController {
+
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      //  scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: 2000)
+
+    }
+    
+
+}
--- Jurni/SettingsViewController.swift
+++ Jurni/SettingsViewController.swift
@@ -6,11 +6,8 @@
 //
 
 import UIKit
-import FirebaseDatabase
-import FirebaseAuth
-import FirebaseFirestore
 
-class SettingsViewController: UIViewController,UITextFieldDelegate {
+class SettingsViewController: UIViewController {
 
     @IBOutlet weak var accountBtn: UIButton!
     @IBOutlet weak var accountImageView: UIImageView!
@@ -24,97 +21,17 @@ class SettingsViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var onboardingImageView: UIImageView!
     @IBOutlet weak var onboardingLabel: UILabel!
     
-    @IBOutlet weak var firstNameTextField: UITextField!
-    @IBOutlet weak var lastNameTextField: UITextField!
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var phoneTextField: UITextField!
-    @IBOutlet weak var jobTitleTextField: UITextField!
-    @IBOutlet weak var hobbiesTextField: UITextField!
-    @IBOutlet weak var currentPwdTextField: UITextField!
-    @IBOutlet weak var newPwdTextField: UITextField!
+
+    @IBOutlet weak var titleLabel: UILabel!
     
+    @IBOutlet weak var containerView: UIView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        firstNameTextField.delegate = self
-        lastNameTextField.delegate = self
-        emailTextField.delegate = self
-        phoneTextField.delegate = self
-        jobTitleTextField.delegate = self
-        hobbiesTextField.delegate = self
-        currentPwdTextField.delegate = self
-        newPwdTextField.delegate = self
-        
-        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
-        view.addGestureRecognizer(tapGesture)
-        
-        fetchUserData()
+       
+        addContactDetailsViewController()
     }
-    
-    func fetchUserData(){
-        let defaultStore: Firestore?
-        defaultStore = Firestore.firestore()
-        let userId : String = Auth.auth().currentUser!.uid
-        let docRef = defaultStore?.collection("users").document(userId)
-        docRef!.getDocument { (document, error) in
-            if let document = document, document.exists {
-                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
-                
-                let firstName = document.get("firstName") as? String
-                if(firstName != nil){
-                    UserDefaults.standard.set(firstName, forKey: Constants.FIRST_NAME)
-                }
-                
-                let lastName = document.get("lastName") as? String
-                if(lastName != nil){
-                    UserDefaults.standard.set(lastName, forKey: Constants.LAST_NAME)
-                }
-                
-                let email = document.get("email") as? String
-                if(email != nil){
-                    UserDefaults.standard.set(email, forKey: Constants.EMAIL)
-                }
-                
-                let phone = document.get("phone") as? String
-                if(phone != nil){
-                    UserDefaults.standard.set(phone, forKey: Constants.PHONE_NUMBER)
-                }
-                
-                let jobTitle = document.get("jobTitle") as? String
-                if(jobTitle != nil){
-                    UserDefaults.standard.set(jobTitle, forKey: Constants.JOB_TITLE)
-                }
-                
-                let avatar = document.get("avatar") as? String
-                if(avatar != nil){
-                    UserDefaults.standard.set(avatar, forKey: Constants.PROFILE_PIC)
-                }
-                
-                let hobbies = document.get("hobbies") as? String
-                if(hobbies != nil){
-                    UserDefaults.standard.set(hobbies, forKey: Constants.HOBBIES)
-                }
-                
-                self.setUserData()
-                
-            } else {
-                print("Document does not exist")
-            }
-        }
-        
-    }
-    
-    func setUserData(){
-        self.firstNameTextField.text = UserDefaults.standard.string(forKey: Constants.FIRST_NAME)
-        self.lastNameTextField.text = UserDefaults.standard.string(forKey: Constants.LAST_NAME)
-        self.emailTextField.text = UserDefaults.standard.string(forKey: Constants.EMAIL)
-        self.phoneTextField.text = UserDefaults.standard.string(forKey: Constants.PHONE_NUMBER)
-        self.jobTitleTextField.text = UserDefaults.standard.string(forKey: Constants.JOB_TITLE)
-        self.hobbiesTextField.text = UserDefaults.standard.string(forKey: Constants.HOBBIES)
-
-    }
-    
     
     @IBAction func backArrowAction(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
@@ -132,7 +49,12 @@ class SettingsViewController: UIViewController,UITextFieldDelegate {
         self.accountLabel.textColor = UIColor(red: 0.061, green: 0.088, blue: 0.107, alpha: 1.0)
         self.paymentLabel.textColor = UIColor.white
         self.onboardingLabel.textColor = UIColor.white
-    
+        
+        titleLabel.text = "Contact Details"
+        
+        removePaymentDetailsViewController()
+        addContactDetailsViewController()
+            
     }
     
     @IBAction func paymentAction(_ sender: Any) {
@@ -147,7 +69,11 @@ class SettingsViewController: UIViewController,UITextFieldDelegate {
         self.accountLabel.textColor = UIColor.white
         self.paymentLabel.textColor = UIColor(red: 0.061, green: 0.088, blue: 0.107, alpha: 1.0)
         self.onboardingLabel.textColor = UIColor.white
+                
+        titleLabel.text = "Billing Information"
         
+        removeContactDetailsViewController()
+        addPaymentDetailsViewController()
     }
     
     @IBAction func onboardingAction(_ sender: Any) {
@@ -162,34 +88,11 @@ class SettingsViewController: UIViewController,UITextFieldDelegate {
         self.accountLabel.textColor = UIColor.white
         self.paymentLabel.textColor = UIColor.white
         self.onboardingLabel.textColor = UIColor(red: 0.061, green: 0.088, blue: 0.107, alpha: 1.0)
-    }
-    
-    @IBAction func saveAction(_ sender: Any) {
-        let firstName : String = firstNameTextField.text ?? ""
-        let lastName : String = lastNameTextField.text ?? ""
-        let email: String = emailTextField.text ?? ""
-        let phone: String = phoneTextField.text ?? ""
-        let jobTitle: String = jobTitleTextField.text ?? ""
-        let hobbies: String = hobbiesTextField.text ?? ""
         
-        
-        let userData: [String: Any] = [
-            "firstName": firstName,
-            "lastName": lastName,
-            "email": email,
-            "phone": phone,
-            "jobTitle":jobTitle,
-            "hobbies": hobbies
-        ]
-        saveUserData(userData: userData)
+        titleLabel.text = ""
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    
+  
     @IBAction func logoutAction(_ sender: Any) {        
         let defaults = UserDefaults.standard
             let dictionary = defaults.dictionaryRepresentation()
@@ -200,23 +103,37 @@ class SettingsViewController: UIViewController,UITextFieldDelegate {
         self.performSegue(withIdentifier: "logoutSegue", sender: nil)
     }
     
-    func saveUserData(userData: [String: Any]){
-        let defaultStore: Firestore?
-        defaultStore = Firestore.firestore()
-        let userId : String = Auth.auth().currentUser!.uid
-        let docRef = defaultStore?.collection("users").document(userId)
-        docRef?.setData(userData){ err in
-            if let err = err {
-                self.showAlert(message: "Error updating Profile. Try again.")
-            } else {
-                self.showAlert(message: "Profile updated successfully")
-            }
+
+    
+    func addContactDetailsViewController(){
+        if let contactDetailsViewController = self.storyboard?.instantiateViewController(identifier: "contactDetailsID") as? ContactDetailsViewController {
+            
+            addChild(contactDetailsViewController)
+            containerView.addSubview(contactDetailsViewController.view)
+        
         }
     }
     
-    func showAlert(message: String){
-        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+    func addPaymentDetailsViewController(){
+        if let paymentDetailsViewController = self.storyboard?.instantiateViewController(identifier: "paymentDetailsID") as? PaymentDetailsViewController {
+            
+            addChild(paymentDetailsViewController)
+            containerView.addSubview(paymentDetailsViewController.view)
+        
+        }
+    }
+    
+    func removeContactDetailsViewController(){
+        if let contactDetailsViewController = self.storyboard?.instantiateViewController(identifier: "contactDetailsID") as? ContactDetailsViewController {
+            contactDetailsViewController.view.removeFromSuperview()
+            contactDetailsViewController.removeFromParent()
+        }
+    }
+    
+    func removePaymentDetailsViewController(){
+        if let paymentDetailsViewController = self.storyboard?.instantiateViewController(identifier: "paymentDetailsID") as? PaymentDetailsViewController {
+            paymentDetailsViewController.view.removeFromSuperview()
+            paymentDetailsViewController.removeFromParent()
+        }
     }
 }
